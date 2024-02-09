3daf318255989162a5ff70547d441844d40cc975
qbnyp <moneycloudy@naver.com>
2024-02-09 16:42:47 +0900

    1

---
+++ styles/index.js
@@ -0,0 +1,10 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    var menuBar = document.getElementById('menuBar');
+    if (menuBar.style.display === 'none') {
+        menuBar.style.display = 'block';
+        this.textContent = '메뉴 닫기';
+    } else {
+        menuBar.style.display = 'none';
+        this.textContent = '메뉴 열기';
+    }
+});

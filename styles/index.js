30fae8eb460d1d7cba2787afb9809232d0b32b55
qbnyp <moneycloudy@naver.com>
2024-02-11 13:31:23 +0900

    필터 form 추가 , search 글자별 찾기 js 추가 , 버튼 누를시 필터 켜지는 기능 추가 , css 조절

--- styles/index.js
+++ styles/index.js
@@ -1,10 +1,24 @@
-document.getElementById('menuButton').addEventListener('click', function() {
-    var menuBar = document.getElementById('menuBar');
-    if (menuBar.style.display === 'none') {
-        menuBar.style.display = 'block';
-        this.textContent = '메뉴 닫기';
-    } else {
-        menuBar.style.display = 'none';
-        this.textContent = '메뉴 열기';
+function form_filter_on(){
+    var targetDiv = document.getElementById('filter_wrapper'); 
+
+    if(targetDiv.style.display === 'none'){
+        targetDiv.style.display = 'inline';
     }
-}); 
+    else if(targetDiv.style.display !== 'none'){
+        targetDiv.style.display = 'none'
+    }
+}
+
+function search_Menu() {
+    var input = document.getElementById('keyword').value;
+    var menus = document.getElementsByClassName('menu');
+
+    for (var i = 0; i < menus.length; i++) {
+        var menuName = menus[i].getElementsByClassName('name')[0].innerText
+        if (menuName.includes(input)) {
+            menus[i].style.display = 'block';
+        } else {
+            menus[i].style.display = 'none';
+        }
+    }
+}
